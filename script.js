1436fa7392183bb8d5ef9c3f775a22bfd37871b4
Vlada Dragun <dragun.vlada.ief@gmail.com>
2020-08-06 14:57:20 +0300

    в функцию добавлено исчезание картинки с абсолютной позицией при открытии меню и ее появление при закрытии меню

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 var menu = document.getElementById('mobile_menu');
 var aside = document.getElementById('aside_menu');
+var wave = document.getElementById('wave');
 
 menu.onclick = function() {
 
   if(aside.style.display == 'flex') {
     aside.style.display = 'none';
+    wave.style.display = 'block';
   }
   else {
     aside.style.display = 'flex';
+    wave.style.display = 'none';
   }
 
 }
