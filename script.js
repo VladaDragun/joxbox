e734c8826d85affa20dd90abdd206fa34a37c832
Vlada Dragun <dragun.vlada.ief@gmail.com>
2020-08-06 13:29:59 +0300

    добавлено адаптивное меню по клику на картинку

---
+++ script.js
@@ -0,0 +1,11 @@
+var menu = document.getElementById('mobile_menu');
+var aside = document.getElementById('aside_menu');
+
+menu.onclick = function() {
+  if(aside.style.display == 'flex') {
+    aside.style.display = 'none';
+  }
+  else {
+    aside.style.display = 'flex';
+  }
+}
